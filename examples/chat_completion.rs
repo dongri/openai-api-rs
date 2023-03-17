5db42ecf0c3545a662a2ed8a135c15b273b18427
Dongri Jin <dongrify@gmail.com>
2023-03-18 06:25:30 +0900

    Add GPT4 models

--- examples/chat_completion.rs
+++ examples/chat_completion.rs
@@ -6,7 +6,7 @@ use std::env;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
     let req = ChatCompletionRequest {
-        model: chat_completion::GPT3_5_TURBO.to_string(),
+        model: chat_completion::GPT4.to_string(),
         messages: vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
             content: String::from("NFTとは？"),
