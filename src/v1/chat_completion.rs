721eb8551ae53efd39fca252120df558e64410b1
Sharif Haason <ssh128@scarletmail.rutgers.edu>
2023-12-27 01:41:28 -0500

    Add the `system_fingerprint` field to the chat completion response

--- src/v1/chat_completion.rs
+++ src/v1/chat_completion.rs
@@ -135,6 +135,7 @@ pub struct ChatCompletionResponse {
     pub model: String,
     pub choices: Vec<ChatCompletionChoice>,
     pub usage: common::Usage,
+    pub system_fingerprint: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
