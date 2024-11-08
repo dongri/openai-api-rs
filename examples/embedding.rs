0ce0d3bbb777d8a0ee9485e82c5f854f1477b76a
Dongri Jin <dongrium@gmail.com>
2024-11-09 08:45:34 +0900

    cargo fmt

--- examples/embedding.rs
+++ examples/embedding.rs
@@ -7,8 +7,10 @@ use std::env;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = OpenAIClient::new(env::var("OPENAI_API_KEY").unwrap().to_string());
 
-    let mut req =
-        EmbeddingRequest::new(TEXT_EMBEDDING_3_SMALL.to_string(), vec!["story time".to_string(), "Once upon a time".to_string()]);
+    let mut req = EmbeddingRequest::new(
+        TEXT_EMBEDDING_3_SMALL.to_string(),
+        vec!["story time".to_string(), "Once upon a time".to_string()],
+    );
     req.dimensions = Some(10);
 
     let result = client.embedding(req).await?;
