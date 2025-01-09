86b7abfd067d7543a3820d4d0fc365d1d3545018
Ramon Iglesias <rdit@stanford.edu>
2025-01-08 18:21:49 -0800

    to string for timestamp granularity

--- src/v1/audio.rs
+++ src/v1/audio.rs
@@ -210,3 +210,12 @@ pub enum TimestampGranularity {
     Word,
     Segment,
 }
+
+impl std::fmt::Display for TimestampGranularity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TimestampGranularity::Word => write!(f, "word"),
+            TimestampGranularity::Segment => write!(f, "segment"),
+        }
+    }
+}
