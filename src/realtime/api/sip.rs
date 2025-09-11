91d9a31b74c61a8a98c1cedbf29b5328b1d96337
janligudzinski <janligudzinski@gmail.com>
2025-09-11 13:19:17 +0200

    add dedicated RealtimeSIP client

--- src/realtime/api/sip.rs
+++ src/realtime/api/sip.rs
@@ -1,33 +1,23 @@
-use futures_util::stream::{SplitSink, SplitStream};
-use futures_util::StreamExt;
-use tokio::net::TcpStream;
-use tokio_tungstenite::{
-    connect_async,
-    tungstenite::{client::IntoClientRequest, protocol::Message},
-    MaybeTlsStream, WebSocketStream,
-};
+use super::*;
 
-pub mod sip;
-
-const WSS_URL: &str = "wss://api.openai.com/v1/realtime";
-
-pub struct RealtimeClient {
+/// Intended for connecting to an already existing Realtime session spawned by accepting an incoming SIP call from e.g. Twilio.
+pub struct RealtimeSipClient {
     pub wss_url: String,
     pub api_key: String,
-    pub model: String,
+    pub call_id: String,
 }
 
-impl RealtimeClient {
-    pub fn new(api_key: String, model: String) -> Self {
+impl RealtimeSipClient {
+    pub fn new(api_key: String, call_id: String) -> Self {
         let wss_url = std::env::var("WSS_URL").unwrap_or_else(|_| WSS_URL.to_owned());
-        Self::new_with_endpoint(wss_url, api_key, model)
+        Self::new_with_endpoint(wss_url, api_key, call_id)
     }
 
-    pub fn new_with_endpoint(wss_url: String, api_key: String, model: String) -> Self {
+    pub fn new_with_endpoint(wss_url: String, api_key: String, call_id: String) -> Self {
         Self {
             wss_url,
             api_key,
-            model,
+            call_id,
         }
     }
 
@@ -40,15 +30,12 @@ impl RealtimeClient {
         ),
         Box<dyn std::error::Error>,
     > {
-        let url = format!("{}?model={}", self.wss_url, self.model);
+        let url = format!("{}?callId={}", self.wss_url, self.call_id);
         let mut request = url.into_client_request()?;
         let api_key = self.api_key.clone();
         request
             .headers_mut()
             .insert("Authorization", format!("Bearer {api_key}").parse()?);
-        request
-            .headers_mut()
-            .insert("OpenAI-Beta", "realtime=v1".parse()?);
         let (ws_stream, _) = connect_async(request).await?;
         let (write, read) = ws_stream.split();
         Ok((write, read))
