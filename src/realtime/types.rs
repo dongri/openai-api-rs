ba122a860add9f6e8210dc93e5fada01f39a49cc
janligudzinski <janligudzinski@gmail.com>
2025-09-12 14:32:49 +0200

    make ItemType PartialEq for filtering

--- src/realtime/types.rs
+++ src/realtime/types.rs
@@ -309,7 +309,7 @@ pub enum MaxOutputTokens {
     Inf,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum ItemType {
     Message,
