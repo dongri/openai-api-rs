7d130cf5f03ccd179519af888fea40a7cfc17514
janligudzinski <janligudzinski@gmail.com>
2025-09-15 18:34:41 +0200

    spacing + doc comm

--- src/v1/realtime_calls.rs
+++ src/v1/realtime_calls.rs
@@ -3,7 +3,8 @@ use serde::{Deserialize, Serialize};
 use crate::realtime::types::Session;
 
 /// Used to start a realtime session based on an incoming call that you can then connect to over WSS with `RealtimeSipClient` from `openai_api_rs::realtime::sip`.
-/// Note that this is poorly documented by OpenAI with the only example data given in https://platform.openai.com/docs/guides/realtime-sip#handle-the-webhook and these may not be all the possible fields.
+/// Note that this is poorly documented by OpenAI with the only example data given in https://platform.openai.com/docs/guides/realtime-sip#handle-the-webhook.
+///
 /// Per an OpenAI dev (https://community.openai.com/t/how-to-setup-transcription-on-realtime-api-with-sip/1355068/12) anything that can be passed to `session.update` over WSS can be passed to /accept,
 /// as well as `model`, ordinarily reserved for `session.create`.
 #[derive(Debug, Serialize, Deserialize, Clone)]
