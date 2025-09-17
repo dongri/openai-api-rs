917a3b6542bf0db095a128bdf9b401f18faa2139
janligudzinski <janligudzinski@gmail.com>
2025-09-17 19:47:19 +0200

    remove model param from realtime endpoint URL

--- src/realtime/api/sip.rs
+++ src/realtime/api/sip.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::realtime::types::RealtimeModel;
-
 use super::*;
 
 /// Intended for connecting to an already existing Realtime session spawned by accepting an incoming SIP call from e.g. Twilio.
@@ -9,26 +7,19 @@ pub struct RealtimeSipClient {
     pub wss_url: String,
     pub api_key: String,
     pub call_id: String,
-    pub model: RealtimeModel, // contrary to the OpenAI tutorial, joining an SIP session without a `model` param causes an "invalid_request_error.missing_model"
 }
 
 impl RealtimeSipClient {
-    pub fn new(api_key: String, call_id: String, model: RealtimeModel) -> Self {
+    pub fn new(api_key: String, call_id: String) -> Self {
         let wss_url = std::env::var("WSS_URL").unwrap_or_else(|_| WSS_URL.to_owned());
-        Self::new_with_endpoint(wss_url, api_key, call_id, model)
+        Self::new_with_endpoint(wss_url, api_key, call_id)
     }
 
-    pub fn new_with_endpoint(
-        wss_url: String,
-        api_key: String,
-        call_id: String,
-        model: RealtimeModel,
-    ) -> Self {
+    pub fn new_with_endpoint(wss_url: String, api_key: String, call_id: String) -> Self {
         Self {
             wss_url,
             api_key,
             call_id,
-            model,
         }
     }
 
@@ -41,18 +32,7 @@ impl RealtimeSipClient {
         ),
         Box<dyn std::error::Error>,
     > {
-        let model_slug = serde_json::to_string(&self.model).unwrap();
-        let model_slug = model_slug
-            .strip_prefix("\"")
-            .unwrap()
-            .strip_suffix("\"")
-            .unwrap();
-
         let url = format!("{}?call_id={}", self.wss_url, self.call_id);
-        // let url = format!(
-        //     "{}?call_id={}&model={}",
-        //     self.wss_url, self.call_id, model_slug
-        // );
         let mut request = url.into_client_request()?;
         let api_key = self.api_key.clone();
         request
