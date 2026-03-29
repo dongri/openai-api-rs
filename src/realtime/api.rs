5e3d3ded495212bd71d063215c8ac2954fb2dd67
janligudzinski <janligudzinski@gmail.com>
2026-03-29 16:51:31 +0200

    remove deprecated Beta header

--- src/realtime/api.rs
+++ src/realtime/api.rs
@@ -46,9 +46,15 @@ impl RealtimeClient {
         request
             .headers_mut()
             .insert("Authorization", format!("Bearer {api_key}").parse()?);
-        request
-            .headers_mut()
-            .insert("OpenAI-Beta", "realtime=v1".parse()?);
+
+        #[deprecated]
+        #[cfg(feature = "realtime_beta_v1")]
+        {
+            request
+                .headers_mut()
+                .insert("OpenAI-Beta", "realtime=v1".parse()?);
+        }
+
         let (ws_stream, _) = connect_async(request).await?;
         let (write, read) = ws_stream.split();
         Ok((write, read))
